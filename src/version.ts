5e64ffcbbd1b00b428b1e1513c27eaaad7ae286c
Liwy <208813904+cutelittlelily@users.noreply.github.com>
2025-05-25 04:05:25 +0200

    so i was in a server right 🐛

--- src/version.ts
+++ src/version.ts
@@ -17,7 +17,7 @@
 import { processCharCodes } from "./ws/util/StringUtil";
 
 /** Current protocol version */
-export const VERSION = 9;
+export const VERSION = 10;
 /** Server data suffix */
 export const SERVER_SUFFIX = "SWS";
 /** Server data suffix in array */
