d263f41c36fe5235563791252828a66e378f7485
Liwy <208813904+cutelittlelily@users.noreply.github.com>
2025-05-25 07:53:36 +0200

    rawr :3

--- release/SonicWS_bundle.js
+++ release/SonicWS_bundle.js
@@ -1,6 +1,6 @@
 /*!
  * SonicWS
- * (c) 2025 Lily (cutelittlelily)
+ * (c) 2025 Lily (liwybloc)
  * Released under the Apache-2.0 License
  * https://www.apache.org/licenses/LICENSE-2.0
  */
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 
 const LICENSE = `/*!
  * SonicWS
- * (c) 2025 Lily (cutelittlelily)
+ * (c) 2025 Lily (liwybloc)
  * Released under the Apache-2.0 License
  * https://www.apache.org/licenses/LICENSE-2.0
  */\n;\n`;
